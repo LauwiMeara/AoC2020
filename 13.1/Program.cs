35186ef77dd6d53f578ebb575b9f28fe3fdf9f3d
LauwiMeara <laurasusanne@live.nl>
2021-01-20 19:11:14 +0100

    Add day 13

---
+++ 13.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _13._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllLines("input.txt");
+
+            int[] busIds = GetBusIds(input);
+            int earliestDepartureTime = int.Parse(input[0]);
+            
+            int[] busWaitingTimes = GetBusWaitingTimes(earliestDepartureTime, busIds);
+
+            int minWaitingTime = busWaitingTimes.Min();
+            int busId = busIds[Array.IndexOf(busWaitingTimes, minWaitingTime)];
+
+            Console.WriteLine($"The bus ID is {busId}.");
+            Console.WriteLine($"The number of minutes you'll have to wait, is {minWaitingTime}.");
+            Console.WriteLine($"These numbers multiplied, is {busId * minWaitingTime}.");
+        }
+
+        static int[] GetBusIds(string[] input)
+        {
+            string[] splitInput = input[1].Replace('x', ',').Split(',', StringSplitOptions.RemoveEmptyEntries);
+            int[] busIds = new int[splitInput.Length];
+
+            for (int i = 0; i < splitInput.Length; i++)
+            {
+                busIds[i] = int.Parse(splitInput[i]);
+            }
+
+            return busIds;
+        }
+
+        static int[] GetBusWaitingTimes(int earliestDepartureTime, int[] busIds)
+        {
+            int[] busWaitingTimes = new int[busIds.Length];
+
+            for (int i = 0; i < busIds.Length; i++)
+            {
+                int remainder = earliestDepartureTime % busIds[i];
+
+                busWaitingTimes[i] = busIds[i] - remainder;
+            }
+
+            return busWaitingTimes;
+        }
+    }
+}
---
+++ 13.2/Program.cs
@@ -0,0 +1,61 @@
+﻿// WARNING: This code takes a long time to get to the end result!
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace _13._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = File.ReadAllText("input.txt").Replace('x', '1').Split(',');
+
+            int[] busIds = GetBusIds(input);
+
+            int highestBusId = busIds.Max();
+            int indexHighestBusId = Array.IndexOf(busIds, highestBusId);
+
+            long timeHighestBusId = 100000000000000 - (100000000000000 % highestBusId); // Start around 100000000000000, as the puzzle states that the consecutive timestamp will be higher
+
+            bool isConsecutive = false;
+
+            while (!isConsecutive)
+            {
+                for (int i = 0; i < busIds.Length; i++)
+                {
+                    int timeDifference = i - indexHighestBusId;
+                    long expectedTime = timeHighestBusId + timeDifference;
+
+                    if (expectedTime % busIds[i] != 0)
+                    {
+                        timeHighestBusId += highestBusId;
+                        break;
+                    }
+
+                    if (i == busIds.Length - 1 && expectedTime % busIds[i] == 0)
+                    {
+                        isConsecutive = true;
+                    }
+                }
+            }
+
+            long consecutiveTimestamp = timeHighestBusId - indexHighestBusId;
+
+            Console.WriteLine($"The consecutive timestamp is {consecutiveTimestamp}.");
+        }
+
+        static int[] GetBusIds(string[] input)
+        {
+            int[] busIds = new int[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                busIds[i] = int.Parse(input[i]);
+            }
+
+            return busIds;
+        }
+    }
+}
